4a654b422693f743f3ea278ec32a020802e8de3b
uziarslan <143803590+uziarslan@users.noreply.github.com>
2024-07-28 07:44:00 +0500

    ready for deploy

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -16,7 +16,7 @@ const protect = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
 
-      const decoded = jwt.verify(token, process.env.jwt_secret);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       let user = await Employee.findById(decoded.id);
 
