12016ae3ab9acb32a448969044f458fe38685ce6
uziarslan <143803590+uziarslan@users.noreply.github.com>
2024-07-28 08:07:27 +0500

    Seed Changes

---
+++ controllers/employeeController.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Employee = mongoose.model("Employee");
+const { mainPage, subPages } = require("../seed/seed");
+
+const verifyCode = async (req, res) => {
+  const { otp, username } = req.body;
+
+  const employee = await Employee.findOne({ username });
+
+  if (!employee || !employee.code) {
+    return res.status(500).json({ error: "Incorrect user or no code found" });
+  }
+
+  if (employee.code !== otp) {
+    return res.status(500).json({ error: "Incorrect Code" });
+  }
+
+  if (employee.code === otp) {
+    return res.status(200).json({ callBack: "/employeeportal" });
+  }
+};
+
+const seedData = async (req, res) => {
+  if (!mainPage || subPages) {
+    return res.status(400).json({ error: "Data not found" });
+  }
+  res.json({ mainPage, subPages });
+};
+
+module.exports = { seedData, verifyCode };
--- routes/employee.js
+++ routes/employee.js
@@ -1,48 +1,11 @@
 const express = require("express");
 const wrapAsync = require("../utils/wrapAsync");
-const mongoose = require("mongoose");
-const Employee = mongoose.model("Employee");
-const jwt = require("jsonwebtoken");
-const { MailtrapClient } = require("mailtrap");
-const { mainPage, subPages } = require("../seed/seed");
-
-const jwt_secret = process.env.jwt_secret;
+const { seedData, verifyCode } = require("../controllers/employeeController");
+const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-const TOKEN = process.env.MAIL_TRAP_TOKEN;
-const ENDPOINT = process.env.MAILTRAP_END_POINT;
-const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
-const sender = {
-  email: "info@bigtristate.com",
-  name: "NYU",
-};
+router.post("/verify-code", wrapAsync(verifyCode));
 
-router.post(
-  "/verify-code",
-  wrapAsync(async (req, res) => {
-    const { otp, username } = req.body;
-
-    const employee = await Employee.findOne({ username });
-
-    if (!employee || !employee.code) {
-      return res.status(500).json({ error: "Incorrect user or no code found" });
-    }
-
-    if (employee.code !== otp) {
-      return res.status(500).json({ error: "Incorrect Code" });
-    }
-
-    if (employee.code === otp) {
-      return res.status(200).json({ callBack: "/employeeportal" });
-    }
-  })
-);
-
-router.get(
-  "/seed",
-  wrapAsync(async (req, res) => {
-    res.json({ mainPage, subPages });
-  })
-);
+router.get("/seed", protect, wrapAsync(seedData));
 
 module.exports = router;
