442dc572304dc73f8466e3db58b7732ac36d6526
uziarslan <143803590+uziarslan@users.noreply.github.com>
2024-07-30 19:01:18 +0500

    Question added

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ const sessionConfig = {
 const corsOptions = {
   origin: process.env.DOMAIN_FRONTEND,
   credentials: true,
-  methods: "GET,POST,PUT,DELETE",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 };
 
